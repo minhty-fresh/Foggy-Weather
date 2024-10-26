87fd2de73299423609e2853572236287fcaca9b9
minhty-fresh <4746495+mdaole@users.noreply.github.com>
2024-10-25 21:30:24 -0700

    initial project startup commit

---
+++ common/src/main/java/com/minhtyfresh/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.minhtyfresh;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "foggy_weather";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
